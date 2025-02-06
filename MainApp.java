2943ba08c0604bbc987ef2833f1644bb8a60a6d9
Yiğit Kaan Önder <kaanonder@ug.bilkent.edu.tr>
2025-02-06 19:49:34 +0300

    MainApp.java
    Updated sumOfEvenAndOddIndexesOfArray method.

--- MainApp.java
+++ MainApp.java
@@ -40,9 +40,25 @@ public class MainApp {
 
     }
 
-    private static void sumOfEvenAndOddIndexesOfArray()
+    /**
+     * This function finds the sum of
+     * elements in odd and even indexes 
+     * seperately. Then prints them.
+     *
+     * @author Yiğit Kaan Önder
+     */
+    private static void sumOfEvenAndOddIndexesOfArray(int[] nums) 
     {
-        
+        int sumOfEven = 0;
+        int sumOfOdd = 0;
+        for (int i = 0; i < nums.length; i+=2) 
+        {
+            sumOfEven += nums[i];
+            sumOfOdd += nums[i + 1];
+        }
+
+        System.out.println("Sum of odd numbers is: " + sumOfOdd);
+        System.out.println("Sum of even numbers is: " + sumOfEven);
     }
 
     

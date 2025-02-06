6b15c0b675e2f1f8c873571a579bcc80e4b20393
Hayati Kocur <h.kocur@hotmail.com>
2025-02-06 19:31:47 +0300

    MainApp.java and the structure of app created.

---
+++ MainApp.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+    private static int[] array;
+
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        
+
+
+
+        in.close();
+    }
+
+    private static void createArray(Scanner in)
+    {
+
+    }
+
+    private static boolean isInitialized()
+    {
+        if(array == null)
+        {
+            System.out.println("Array didn't created. Create an array before doing an operation.");
+        }
+
+        return true;
+    }
+
+    private static void findMaxAndMin()
+    {
+
+
+    }
+
+    private static void averageAndDiffs()
+    {
+
+    }
+
+    private static void sumOfEvenAndOddIndexesOfArray()
+    {
+        
+    }
+
+    
+}
